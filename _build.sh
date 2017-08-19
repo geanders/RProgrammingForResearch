65e8b733bb6c991577356eb47301682506573be2
Brooke Anderson <brooke.anderson@colostate.edu>
2017-08-18 19:08:47 -0600

    Try again for basic Travis build

--- _build.sh
+++ _build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
